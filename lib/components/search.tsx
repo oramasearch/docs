70b85d9575aeca7d2f3e852db5d4f0bb1769105d
Aileen Villanueva <aileenvl@gmail.com>
2025-05-29 21:43:03 -0600

    update collection and fix open search behaviour

--- lib/components/search.tsx
+++ lib/components/search.tsx
@@ -1,13 +1,41 @@
 import { OramaSearchBox } from "@orama/react-components";
 import { CollectionManager } from "@orama/core";
 import type { SharedProps } from 'fumadocs-ui/components/dialog/search';
+import { useEffect } from 'react';
 
 export default function Search(props: SharedProps) {
   const clientInstance = new CollectionManager({
     url: 'https://collections.orama.com',
-    collectionID: 'ktqngtxsz0y1s978hpykyz96',
-    readAPIKey: 'FgHteRBsizAFBjmDi7Cz7WlAl244L1nR',
-  })
-  return <OramaSearchBox {...props} oramaCoreClientInstance={clientInstance} />;
+    collectionID: 'lnyr5uc7v7bievfrivgvony1',
+    readAPIKey: 'dyBSIl7O5TEXOZ7KfnNpxCaZ7ZlekZKm',
+  });
 
+  const { open, onOpenChange, ...restProps } = props;
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        if (open) {
+          onOpenChange(false);
+        }
+      }
+    };
+
+    if (open) {
+      document.addEventListener('keydown', handleKeyDown, true);
+    }
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown, true);
+    };
+  }, [open, onOpenChange]);
+
+  return (
+    <OramaSearchBox
+      open={open}
+      oramaCoreClientInstance={clientInstance}
+      colorScheme="system"
+      {...restProps}
+    />
+  );
 }
