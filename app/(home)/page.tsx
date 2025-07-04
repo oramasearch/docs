e55bf0384955cab788f19d85c8c46f4a3e1faaca
Francesca Giannino <francesca.giannino86@gmail.com>
2025-07-04 16:15:59 +0200

    style: adjust hero responsive

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,43 +5,47 @@ import Link from 'next/link'
 
 export default function HomePage() {
   return (
-    <div>
+    <>
       <Hero
         title='Smarter search starts here'
         description='Orama gives you everything you need to create fast, powerful, and flexible search experiences.From full-text to vector search, local to cloud, all backed by modern AI.'
       />
       <section>
-        <div className='mx-auto max-w-fd-container flex flex-col justify-center items-center px-4'>
-          <SmartSearch />
-        </div>
-      </section>
-      <section className='my-10 lg:my-20 py-12 text-center'>
-        <div className='mx-auto max-w-fd-container flex flex-col justify-center items-center px-4'>
-          <h2 className='text-2xl xl:text-4xl leading-none text-foreground my-3 font-extrabold'>
-            Getting started
-          </h2>
-          <p className='text-xs lg:text-lg leading-normal text-muted-foreground'>
-            Check out our docsand dive into your next Orama Experience
-          </p>
-          <div className='mt-6 w-full flex flex-col items-center gap-3 lg:flex-row lg:gap-6 lg:justify-center'>
-            <Link
-              href='#'
-              className='max-w-60 w-full block py-2 px-4 text-center rounded-md bg-primary text-primary-foreground hover:bg-fd-base-primary/90 transition-colors duration-200 shadow-md shadow-fd-base-primary/20'
-            >
-              Get started
-            </Link>
-            <a
-              href='#update-me'
-              className='w-full max-w-60 flex items-center justify-center py-2 px-4 text-center rounded-md bg-input/30 text-foreground hover:bg-fd-base-primary/90 transition-colors duration-200 shadow-md shadow-fd-base-primary/20'
-              rel='noopener noreferrer'
-              target='_blank'
-            >
-              View on GitHub
-              <GithubIcon className='inline ml-2 size-4' />
-            </a>
+        <div className='mx-auto max-w-fd-container flex flex-col justify-center items-center'>
+          <div className='text-center max-w-[960px] w-90/100 md:w-80/100 lg:w-90/100'>
+            <SmartSearch />
           </div>
         </div>
       </section>
-    </div>
+      <section>
+        <div className='mx-auto max-w-fd-container flex flex-col justify-center items-center'>
+          <div className='text-center max-w-[960px] w-90/100 md:w-80/100 lg:w-90/100'>
+            <h2 className='text-2xl xl:text-4xl leading-none text-foreground my-3 font-extrabold'>
+              Getting started
+            </h2>
+            <p className='text-xs lg:text-lg leading-normal text-muted-foreground'>
+              Check out our docsand dive into your next Orama Experience
+            </p>
+            <div className='mt-6 w-full flex flex-col items-center gap-3 lg:flex-row lg:gap-6 lg:justify-center'>
+              <Link
+                href='#'
+                className='max-w-60 w-full block py-2 px-4 text-center rounded-md bg-primary text-primary-foreground hover:bg-fd-base-primary/90 transition-colors duration-200 shadow-md shadow-fd-base-primary/20'
+              >
+                Get started
+              </Link>
+              <a
+                href='#update-me'
+                className='w-full max-w-60 flex items-center justify-center py-2 px-4 text-center rounded-md bg-input/30 text-foreground hover:bg-fd-base-primary/90 transition-colors duration-200 shadow-md shadow-fd-base-primary/20'
+                rel='noopener noreferrer'
+                target='_blank'
+              >
+                View on GitHub
+                <GithubIcon className='inline ml-2 size-4' />
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
--- lib/components/hero.tsx
+++ lib/components/hero.tsx
@@ -5,17 +5,19 @@ type HeroProps = {
 
 export function Hero(props: HeroProps) {
   return (
-    <div className='mx-auto max-w-fd-container flex flex-col justify-center my-10 xl:my-20'>
-      <div className='mx-auto max-w-5xl w-90/100 md:w-80/100 lg:w-90/100 pt-10 xl:pt-20 text-center'>
-        <h1 className='text-3xl md:text-6xl lg:text-7xl font-extrabold text-foreground'>
-          {props.title}
-        </h1>
-        {props.description && (
-          <p className='text-sm md:text-lg leading-normale font-normal text-muted-foreground mt-3 xl:mt-6'>
-            {props.description}
-          </p>
-        )}
+    <section>
+      <div className='mx-auto max-w-fd-container flex flex-col justify-center'>
+        <div className='mx-auto max-w-[960px] w-90/100 md:w-80/100 lg:w-90/100 pb-10 pt-12 md:pt-20 lg:pt-30 lg:px-24 text-center'>
+          <h1 className='text-3xl md:text-6xl lg:text-7xl font-extrabold text-foreground'>
+            {props.title}
+          </h1>
+          {props.description && (
+            <p className='text-sm md:text-lg leading-normale font-normal text-muted-foreground mt-3 lg:mt-6'>
+              {props.description}
+            </p>
+          )}
+        </div>
       </div>
-    </div>
+    </section>
   )
 }
