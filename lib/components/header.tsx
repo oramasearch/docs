4b9d7b343eb45b5b0cc0400f5ce82776c7c4f28d
Francesca Giannino <francesca.giannino86@gmail.com>
2025-08-06 15:05:11 +0200

    feat: add custom header hp component

---
+++ lib/components/header.tsx
@@ -0,0 +1,16 @@
+export function Header() {
+  return (
+    <header className='fixed top-(--fd-banner-height) z-40 box-content backdrop-blur-lg w-full transition-colors lg:[--fd-padding:1rem] py-4 shadow-sm border-b border-base'>
+      <div className='mx-auto max-w-fd-container flex flex-col lg:flex-row items-center justify-between'>
+        <div className='flex items-center text-base'>
+          <img
+            src='/logo/orama-logo.svg'
+            className='h-6 mr-2'
+            alt='OramaSearch Inc. Logo'
+          />
+          Orama
+        </div>
+      </div>
+    </header>
+  )
+}
