72ff77a4a894f41e65c188b10e65e14d88bac665
Francesca Giannino <francesca.giannino86@gmail.com>
2025-11-10 17:56:35 +0100

    fix: chat sources content

--- lib/components/Searchbox/chat-sources.tsx
+++ lib/components/Searchbox/chat-sources.tsx
@@ -1,6 +1,7 @@
 import type { Interaction, AnyObject } from '@orama/core'
 import { ChatInteractions } from '@orama/ui/components'
 import type { FC } from 'react'
+import { Link2 } from 'lucide-react'
 
 import type { Document } from './document-link'
 import { DocumentLink } from './document-link'
@@ -31,11 +32,15 @@ export const ChatSources: FC<ChatSourcesProps> = ({ interaction }) => {
               className='w-3xs rounded-xl bg-neutral-100 px-4 py-2 text-neutral-900 duration-300 hover:bg-neutral-200 focus:bg-neutral-200 focus:outline-none motion-safe:transition-colors dark:bg-neutral-900 dark:text-neutral-200 hover:dark:bg-neutral-700 focus:dark:bg-neutral-700'
               data-focus-on-arrow-nav
             >
-              <span className='max-w-full overflow-hidden truncate text-ellipsis whitespace-nowrap text-sm font-semibold'>
-                {document.title && document.title.length > 25
-                  ? `${document.title.substring(0, 25)}...`
-                  : document.title}
-              </span>
+              <h3 className='text-sm flex gap-1 font-semibold text-secondary-foreground line-clamp-2 items-center'>
+                <Link2 className='size-4' />
+                {(document.title as string).slice(0, 30) || ''}
+                {(document.title as string).length > 30 ? '...' : ''}
+              </h3>
+              <p className='text-muted-foreground text-xs mt-1 line-clamp-2'>
+                {(document.content as string).slice(0, 60)}
+                ...
+              </p>
             </DocumentLink>
           )}
         </div>
